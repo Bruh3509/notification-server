287bef80619e633dfb4e27730a207f49e90b323d
Bruh3509 <iutrushkin@bk.ru>
2024-09-04 11:12:15 +0300

    init

---
+++ src/main/java/com/demo/notification_server/NotificationServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.notification_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServerApplication.class, args);
+	}
+
+}
